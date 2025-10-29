bc31647d8bd8c95a053dfe7f7a8a7855ca78934e
Zanooon97 <107140533+Zanooon97@users.noreply.github.com>
2025-10-29 09:31:01 +0100

    Update config_flow.py

--- custom_components/tadox_integration/config_flow.py
+++ custom_components/tadox_integration/config_flow.py
@@ -1,10 +1,23 @@
 from homeassistant.helpers import config_entry_oauth2_flow
+import logging
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 class TadoXFlowHandler(config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN):
-    """Handle the OAuth2 flow."""
+    """Handle den OAuth2 Login-Flow für Tado X."""
 
     DOMAIN = DOMAIN
 
+    @property
+    def logger(self):
+        """Pflicht-Methode: gibt den Logger zurück."""
+        return _LOGGER
+
     async def async_oauth_create_entry(self, data):
-        return self.async_create_entry(title="Tado X Account", data=data)
+        """Wird aufgerufen, wenn der Login erfolgreich war."""
+        return self.async_create_entry(
+            title="Tado X Account",
+            data=data,
+    
+        )
